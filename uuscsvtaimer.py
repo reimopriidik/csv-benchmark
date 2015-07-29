b35d3e5bacd938addb71dfe322b59c352e21640d
reimopriidik <reimopriidik@gmail.com>
2015-07-29 10:26:43 +0300

    Update uuscsvtaimer.py

--- uuscsvtaimer.py
+++ uuscsvtaimer.py
@@ -10,84 +10,84 @@ from pandas import read_csv, read_table
 #chdir('/home/r/python')
 inputFile = ('rand100,5.csv')
 
-def avamistaimer(a, e):
+def avamistaimer(avamisviis, alh):
 	result=[]
 	for _ in range(99):
 		start=timeit.default_timer()
-		if e=='ava':
-			avamisf(a)
-		elif e=='loop':
-			loopf(a)
-		elif e=='header':
-			headerf(a)
+		if alh=='ava':
+			avamisf(avamisviis)
+		elif alh=='loop':
+			loopf(avamisviis)
+		elif alh=='header':
+			headerf(avamisviis)
 		end=timeit.default_timer()
 		result.append(round((end - start), 7))
-	f.close()
-	kirj(e, result)
+	open(inputFile,'r').close()
+	kirj(alh, result)
 
-def kirj(e, result):
-	if e=='ava':
+def kirj(alh, result):
+	if alh=='ava':
 		with open('avamine_ajad.json', 'a') as outfile:
 			json.dump(result, outfile)
-	elif e=='loop':
+	elif alh=='loop':
 		with open('loop_ajad.json', 'a') as outfile:
 			json.dump(result, outfile)
-	elif e=='header':
+	elif alh=='header':
 		with open('header_ajad.json', 'a') as outfile:
 			json.dump(result, outfile)
 
-def avamisf(a):
-	if a==csv.reader or a==csv.DictReader:
+def avamisf(avamisviis):
+	if avamisviis==csv.reader or avamisviis==csv.DictReader:
 		with open(inputFile,'r') as csvfile:
-			a(csvfile);pass
-	elif a==genfromtxt or a==loadtxt:
+			avamisviis(csvfile);pass
+	elif avamisviis==genfromtxt or avamisviis==loadtxt:
 		with open(inputFile, 'r') as csvfile:
-			a(csvfile, dtype = str)
-	elif a==read_csv or a==read_table:
+			avamisviis(csvfile, dtype = str)
+	elif avamisviis==read_csv or avamisviis==read_table:
 		with open(inputFile, 'r') as csvfile:
-			a(csvfile, index_col=False, header=0);pass
+			avamisviis(csvfile, index_col=False, header=0);pass
 
-def loopf(a):
-	if a==csv.reader or a==csv.DictReader:
+def loopf(avamisviis):
+	if avamisviis==csv.reader or avamisviis==csv.DictReader:
 		with open(inputFile, 'r') as csvfile:
-			for row in a(csvfile):
+			for row in avamisviis(csvfile):
 				pass
-	elif a==genfromtxt or a==loadtxt:
+	elif avamisviis==genfromtxt or avamisviis==loadtxt:
 		with open(inputFile, 'r') as csvfile:
-			for row in a(csvfile, dtype = str):
+			for row in avamisviis(csvfile, dtype = str):
 				pass
 
-def headerf(a):
-	if a==csv.reader or a==csv.DictReader:
+def headerf(avamisviis):
+	if avamisviis==csv.reader or avamisviis==csv.DictReader:
 		with open(inputFile, 'r') as csvfile:
-			r1=(a(csvfile))
+			r1=(avamisviis(csvfile))
 			r1.next()
 			pass
-	elif a==genfromtxt or a==loadtxt:
+	elif avamisviis==genfromtxt or avamisviis==loadtxt:
 		with open(inputFile, 'r') as csvfile:
-			a(csvfile, dtype = str)['0']
+			avamisviis(csvfile, dtype = str)['0']
 			pass
-	elif a==read_csv or a==read_table:
+	elif avamisviis==read_csv or avamisviis==read_table:
 		with open(inputFile, 'r') as csvfile:
 			l1 = read_csv(csvfile, delimiter=(','), header=0)
 			list(l1.columns.values)
 			pass
 
-avamistaimer(a=csv.reader, e='ava')
-avamistaimer(a=csv.DictReader, e='ava')
-avamistaimer(a=genfromtxt, e='ava')
-avamistaimer(a=loadtxt, e='ava')
-avamistaimer(a=read_csv, e='ava')
-avamistaimer(a=read_table, e='ava')
+avamistaimer(avamisviis=csv.reader, alh='ava')
+avamistaimer(avamisviis=csv.DictReader, alh='ava')
+avamistaimer(avamisviis=genfromtxt, alh='ava')
+avamistaimer(avamisviis=loadtxt, alh='ava')
+avamistaimer(avamisviis=read_csv, alh='ava')
+avamistaimer(avamisviis=read_table, alh='ava')
 
-avamistaimer(a=csv.reader, e='loop')
-avamistaimer(a=csv.DictReader, e='loop')
-avamistaimer(a=genfromtxt, e='loop')
-avamistaimer(a=loadtxt,e='loop')
+avamistaimer(avamisviis=csv.reader, alh='loop')
+avamistaimer(avamisviis=csv.DictReader, alh='loop')
+avamistaimer(avamisviis=genfromtxt, alh='loop')
+avamistaimer(avamisviis=loadtxt,alh='loop')
 
-avamistaimer(a=csv.reader, e='header')
-avamistaimer(a=csv.DictReader, e='header')
-avamistaimer(a=genfromtxt, e='header')
-avamistaimer(a=loadtxt, e='header')
-avamistaimer(a=read_csv, e='header')
-avamistaimer(a=read_table, e='header')
+avamistaimer(avamisviis=csv.reader, alh='header')
+avamistaimer(avamisviis=csv.DictReader, alh='header')
+avamistaimer(avamisviis=genfromtxt, alh='header')
+avamistaimer(avamisviis=loadtxt, alh='header')
+avamistaimer(avamisviis=read_csv, alh='header')
+avamistaimer(avamisviis=read_table, alh='header')
